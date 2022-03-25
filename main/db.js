07dfb231d316b0b8184b901671ac5223f5677935
Rohail Babar <rohailbabar1997@gmail.com>
2022-03-26 03:34:35 +0500

    fix

--- main/db.js
+++ main/db.js
@@ -10,7 +10,7 @@ const pool = new Pool({
   database: config.database,
   password: config.password,
   port: config.port,
-  ssl: "require"
+  ssl: { rejectUnauthorized: false }
 })
 
 module.exports = pool
