e48a93c12cc9f661a658fae3c6b9bc26323309d1
Rohail Babar <rohailbabar1997@gmail.com>
2022-03-29 00:07:32 +0500

    updates

---
+++ README.md
@@ -0,0 +1,2 @@
+Full Stack Blog Application in development
+LINK: https://blog-reactnode.herokuapp.com
--- client/src/functional/home.js
+++ client/src/functional/home.js
@@ -6,10 +6,22 @@ const Home = (props) => (
     <div>
       <h1>Welcome to The Blog</h1>
       <h2>Get Started</h2>
+      {
+      !props.is_authenticated ? 
       <Button color="primary" size="large" variant="contained" onClick={() => props.auth.login()}>
       Signup/Login
+    </Button>:
+    <Button color="primary" size="large" variant="contained" onClick={() => props.history.replace('/posts')}>
+      FORUM
     </Button>
+    }
     </div>
 );
 
-export default Home;
+function mapStateToProps(state) {
+  return {
+    is_authenticated: state.auth_reducer.is_authenticated
+  }
+}
+
+export default connect(mapStateToProps)(Home);
